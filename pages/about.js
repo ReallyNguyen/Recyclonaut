8b24980b08058edd1a43ac6286c3b3dfabfa4b7b
ReallyNguyen <jnguyen188@my.bcit.ca>
2023-04-21 00:03:39 -0700

    env set up

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,14 @@ import NavBar from '@/components/NavBar'
 import Image from 'next/image'
 
 export default function About() {
+
+    var jordan = process.env.NEXT_PUBLIC_NAME_JORDAN;
+    var mari = process.env.NEXT_PUBLIC_NAME_MARIELA;
+    var front = process.env.NEXT_PUBLIC_ROLE_FRONT;
+    var ui = process.env.NEXT_PUBLIC_ROLE_UI;
+    var introJordan = process.env.NEXT_PUBLIC_INTRODUCTION_JORDAN;
+    var introMari = process.env.NEXT_PUBLIC_INTRODUCTION_MARIELA;
+
     return (
         <>
             <div className={styles.container}>
@@ -11,12 +19,11 @@ export default function About() {
                     <Image src="/about/Jordan.svg" alt="Jordan" className={styles.card__image} width={100} height={100} />
                     <div className={styles.card__body}>
                         <div className={styles.card__name}>
-                            <h2 className={styles.card__title}>Jordan</h2>
-                            <h4 className={styles.card__subtitle}>Front End Developer</h4>
+                            <h2 className={styles.card__title}>{jordan}</h2>
+                            <h4 className={styles.card__subtitle}>{front}</h4>
                         </div>
                         <p className={styles.card__description}>
-                            Hi there! I'm Jordan, a dynamic app developer and designer who loves collaborating with
-                            others and has a knack for finding humor in every situation.
+                            {introJordan}
                         </p>
                     </div>
                 </div>
@@ -25,13 +32,12 @@ export default function About() {
                     <Image src="/about/Mari.svg" alt="Mari" className={styles.card__image} width={100} height={100} />
                     <div className={styles.card__body}>
                         <div className={styles.card__name}>
-                            <h2 className={styles.card__title}>Mariela</h2>
-                            <h5 className={styles.card__subtitle}>UX/UI Designer</h5>
-                            <h5 className={styles.card__subtitle}>Front End Developer</h5>
+                            <h2 className={styles.card__title}>{mari}</h2>
+                            <h5 className={styles.card__subtitle}>{ui}</h5>
+                            <h5 className={styles.card__subtitle}>{front}</h5>
                         </div>
                         <p className={styles.card__description}>
-                            Hey, I'm Mariela, a UX/UI designer and developer that has a passion for thinking creatively
-                            and paying close attention to detail
+                            {introMari}
                         </p>
                     </div>
                 </div>
